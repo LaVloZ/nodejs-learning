378525fa02259fee892afc0a82039ecb0f6174c5
Abdessamed MANSOURI <mansouri.abdessamed@gmail.com>
2023-04-01 01:54:04 +0200

    testing knex with a real database connection

--- test/test.ts
+++ test/test.ts
@@ -1,7 +1,21 @@
 import request from 'supertest'
 import app from '../src/index'
+
+const mdb = require('knex-mariadb');
+const knex = require('knex')({
+    client: mdb,
+    connection: {
+        host : '127.0.0.1',
+        port : 3306,
+        user : 'root',
+        password : '0000',
+        database : 'nodejs'
+      }
+  });
   
-test('adds 1 + 2 to equal 3', async () => {
-    const response = await request(app).get('/');
-    expect(response.status).toBe(200);
+test('read username from database and it should be merrill', async () => {
+    const users = await knex.select('nickname')
+        .from('user')
+
+        expect(users[0].nickname).toBe('merrill')
 });
