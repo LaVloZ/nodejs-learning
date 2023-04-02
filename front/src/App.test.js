6306b0053bd8cfadafc2b420966dc88373c00031
Abdessamed MANSOURI <mansouri.abdessamed@gmail.com>
2023-04-02 06:58:24 +0200

    testing show product name

--- front/src/App.test.js
+++ front/src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('show product title : salade césarienne', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  
+  const linkElement = screen.getByText('Salade césarienne')
   expect(linkElement).toBeInTheDocument();
 });
