27d21a24f2249b37a6b44a276d812a7252a199af
Abdessamed MANSOURI <mansouri.abdessamed@gmail.com>
2023-04-02 03:05:04 +0200

    adding salade picture

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import salade from './salade.webp'
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
             Salade césarienne
           </div>
           <div>
-            picture
+            <img src={salade} />
           </div>
           <div>
             5,00 €
