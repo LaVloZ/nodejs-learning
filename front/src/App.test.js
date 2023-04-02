70d1479de6b150c51fb23aecab76b3e8cc194f57
Abdessamed MANSOURI <mansouri.abdessamed@gmail.com>
2023-04-02 07:00:46 +0200

    testing product price

--- front/src/App.test.js
+++ front/src/App.test.js
@@ -3,7 +3,22 @@ import App from './App';
 
 test('show product title : salade césarienne', () => {
   render(<App />);
-  
+
   const linkElement = screen.getByText('Salade césarienne')
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+test('show product price : 5,00 €', () => {
+  render(<App />);
+  
+  const linkElement = screen.getByText('5,00 €')
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('show product price : 5,00 €', () => {
+  render(<App />);
+  
+  const linkElement = screen.getByText('5,00 €')
   expect(linkElement).toBeInTheDocument();
 });
